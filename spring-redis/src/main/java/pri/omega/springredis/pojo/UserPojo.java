e349afb9172ebee2ef0f7005f78a3a422db0a551
zexuan.li <924900671@qq.com>
2022-04-05 14:53:38 +0800

    修改

---
+++ spring-redis/src/main/java/pri/omega/springredis/pojo/UserPojo.java
@@ -0,0 +1,11 @@
+package pri.omega.springredis.pojo;
+
+import lombok.Data;
+
+@Data
+public class UserPojo {
+    private String id;
+    private String name;
+    private String age;
+    private String sex;
+}
