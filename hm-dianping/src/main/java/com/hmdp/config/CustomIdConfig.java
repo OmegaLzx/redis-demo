0a9493889df2194678fd8c078eeed58fa1d3619f
zexuan.li <924900671@qq.com>
2022-08-07 18:30:28 +0800

    fix

--- hm-dianping/src/main/java/com/hmdp/config/CustomIdConfig.java
+++ hm-dianping/src/main/java/com/hmdp/config/CustomIdConfig.java
@@ -3,6 +3,7 @@ package com.hmdp.config;
 import com.baomidou.mybatisplus.core.incrementer.IdentifierGenerator;
 import com.hmdp.utils.RedisIdWorker;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @RequiredArgsConstructor
@@ -10,9 +11,10 @@ import org.springframework.context.annotation.Configuration;
 public class CustomIdConfig {
     private final RedisIdWorker redisIdWorker;
 
+    @Bean
     public IdentifierGenerator idGenerator() {
         return entity -> {
-            String bizKey = entity.getClass().getName();
+            String bizKey = entity.getClass().getSimpleName().toLowerCase();
             return redisIdWorker.nextId(bizKey);
         };
     }
